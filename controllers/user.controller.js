298a2baba847510f69bb535678bae0818cb2dfc0
Glitch (fir-nettle-soybean) <none>
2020-05-17 03:30:59 +0000

    💗💫 Updated with Glitch add pagination

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,18 @@
-const db = require("../db.js")
+const db = require("../db.js");
 const shortid = require("shortid");
 
-
 module.exports.index = function(req, res) {
+  var page = parseInt(req.query.page) || 1;
+  var perPage = 8;
+  var start = (page - 1) * perPage;
+  var end = page * perPage;
   res.render("users/users", {
-    users: db.get("users").value()
+    users: db
+      .get("users")
+      .value()
+      .slice(start, end)
   });
-}
+};
 
 module.exports.postIndex = function(req, res) {
   req.body.id = shortid.generate();
@@ -14,14 +20,14 @@ module.exports.postIndex = function(req, res) {
     .push(req.body)
     .write();
   res.redirect("back");
-}
+};
 
 module.exports.getUpdate = function(req, res) {
   var id = req.params.id;
   res.render("users/edit-name", {
     id: id
   });
-}
+};
 
 module.exports.postUpdate = function(req, res) {
   db.get("users")
@@ -29,7 +35,7 @@ module.exports.postUpdate = function(req, res) {
     .assign({ name: req.body.name })
     .write();
   res.redirect("/users");
-}
+};
 
 module.exports.getDelete = function(req, res) {
   var id = req.params.id;
@@ -37,4 +43,4 @@ module.exports.getDelete = function(req, res) {
     .remove({ id: id })
     .write();
   res.redirect("back");
-}
+};
